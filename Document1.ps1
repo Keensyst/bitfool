d106282cc727fefac3fde0952ffa9e4efb7e08a8
Walter Keeney <34424226+Keensyst@users.noreply.github.com>
2018-01-21 01:38:28 -0600

    Add files via upload

---
+++ Document1.ps1
@@ -0,0 +1,38 @@
+{
+"packageName":"copay"
+"packageDescription":"CopayBitcoinWallet",
+"packageNameID": "com.bitpay,copay",
+"statusBarColor":#192c3a",
+"userVisibleName":"Copay",
+"purpoeLine":"copayBitcoinWallet",
+"bundleName":"copay",
+"appUri":"copay",
+"name":"Copay",
+"nameNoSPACE":"copay",
+"nameCase":"Copay
+"nameCaseNoSpace":"Copay",
+"githubRepoName","copay",
+"githHubRepoUr1":"git://github.com/bitpay/copay.git",
+"gitHubRepoBugs":"https://github.com/bitpay/copay/issues",
+"disclaimerUr1":"https://copay.io/disclaimer",
+ur1:"https://"copay.io",
+"appDescription":"CopayBitcoinWallrt",
+"inAppname":"CopayWallet",
+WindowsStoreIdentityName":"18c7659D.Copay-SecureBitcoinWallet",
+"WindowsStoreDisplayName":"Copay-SecureBitcoinWallet",
+"windowsAppId":"804636ee- b017-4cad-8719-e58ac97ffa5c,
+'pushSenderId":"103694813229",
+"description":"ASecureBitcoinWallet",
+"version":"3.11.1",
+"androidVersion":"311100",
+"_extraCSS",null,
+"_enableExtensions":{
+"	coin":TRUE,
+"glidera":TRUE,
+"amozon":TRUE
+  }
+  }
+
+}
+
+'"
